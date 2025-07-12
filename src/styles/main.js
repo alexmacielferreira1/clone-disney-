5be03e61d0f617cc66a6bec6b866f1cb1ab0d24b
alexmacielferreira1 <alexmacielferreira@gmail.com>
2025-07-12 15:44:19 -0300

    CABEÇALHO

--- src/styles/main.js
+++ src/styles/main.js
@@ -27,6 +27,7 @@ function exibeElementoDoHeader() {
     header.classList.remove('header--is-hidden');
 }
 
+//Seção de atrações, programacao das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -39,6 +40,7 @@ function exibeElementoDoHeader() {
         });
     }
 
+    //Seção FAQ, accordion
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
